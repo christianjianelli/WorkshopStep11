ad721b838a24d73a440c6978fc2421494e016445
Jianelli Silva Christian <christianjianelli@gmail.com>
2019-04-30 09:06:12 +0000

    First version

---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,37 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History",
+	"sap/ui/core/UIComponent"
+], function (Controller, History, UIComponent) {
+	"use strict";
+	return Controller.extend("com.cgi.workshop.fiori.controller.Detail", {
+		
+		onInit: function () {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
+		},
+		
+		_onObjectMatched: function (oEvent) {
+
+			this.getView().bindElement({
+				path: "/Customers('" + oEvent.getParameter("arguments").objectKey + "')"
+			});
+
+		},
+		
+		onNavBack: function () {
+
+			var oHistory = History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				var oRouter = UIComponent.getRouterFor(this);
+				oRouter.navTo("master", {}, true);
+			}
+
+		}
+
+	});
+});
---
+++ webapp/controller/Master.controller.js
@@ -0,0 +1,83 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
+	"use strict";
+	return Controller.extend("com.cgi.workshop.fiori.controller.Master", {
+
+		_actionPerformed: "",
+
+		_getDialog: function () {
+			if (!this._oDialog) {
+				this._oDialog = sap.ui.xmlfragment("com.cgi.workshop.fiori.view.ConfirmDialog", this);
+				this.getView().addDependent(this._oDialog);
+			}
+			return this._oDialog;
+		},
+
+		onOpenDialog: function () {
+			this._getDialog().open();
+		},
+
+		onCloseDialog: function () {
+			this._getDialog().close();
+		},
+
+		onRefresh: function () {
+			this._actionPerformed = "refresh";
+			this.onOpenDialog();
+		},
+
+		onDelete: function () {
+			this._actionPerformed = "delete";
+			this.onOpenDialog();
+		},
+
+		onConfirm: function () {
+
+			this.onCloseDialog();
+
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+
+			if (this._actionPerformed === "refresh") {
+				
+				//MessageToast.show(oBundle.getText("refreshConfirmed"));
+				
+				this.getView().getModel().refresh();
+				
+			}
+
+			if (this._actionPerformed === "delete") {
+				
+				MessageToast.show(oBundle.getText("deleteConfirmed"));
+				
+				//this.getView().getModel().remove("/Customers('ALFKI')");
+				
+			}
+		},
+
+		onDisplay: function () {
+
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+
+			var oView = this.getView();
+
+			var oList = oView.byId("customerList");
+			
+			var oItem = oList.getSelectedItem();
+
+			if (oItem !== null) {
+
+				oRouter.navTo("detail", {
+					objectKey: oItem.getDescription()
+				});
+
+			} else {
+				
+				MessageToast.show("Please select a Customer");
+				
+			} 
+
+		}
+	});
+});
